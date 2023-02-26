de96722b20360918eb4a934c561e8960dae0217e
Ross Tarrant <rosstarrant3@gmail.com>
2023-02-26 15:03:31 +0000

    Header and Skills to functional components

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,69 +1,57 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import '../styles/Skills.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare, faTrash , faSquareCheck, faStar } from '@fortawesome/free-solid-svg-icons';
 import uniqid from "uniqid";
 
-class Skills extends Component{
+export default function Skills(props){
 
-    constructor(props){
-        super(props)
+    const [skillInput, setSkillInput] = useState('');
+    const [starsInput, setStarsInput] = useState(5);
+    const [skills, setSkills] = useState([]);
+    const [edit, setEdit] = useState(false);
 
-        this.state = {
-            skillInput: '',
-            starsInput: 5,
-            skills: [],
-            edit: false,
-        };
-    }
+    useEffect( () => {
+        if(props.autofill){
+            setSkills([
+                {
+                id: uniqid(),
+                skill:"HTML",
+                stars: 3,
+                edit: false,
+            }, 
+            {
+                id: uniqid(),
+                skill: "CSS",
+                stars: 3,
+                edit: false,
+            },
+            {
+                id: uniqid(),
+                skill: "React.js",
+                stars: 5,
+                edit: false,
+            },
+            {
+                id: uniqid(),
+                skill: "Javascript",
+                stars: 5,
+                edit: false,
+            },
+            {
+                id: uniqid(),
+                skill: "Python",
+                stars: 2,
+                edit: false,
+            }
+        ])
+           }
+           else{
+            setSkills([]);
+           }
+    }, [props.autofill])
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.autofill !== this.props.autofill) {
-               if(this.props.autofill){
-                this.setState({
-                    skills: [
-                        {
-                        id: uniqid(),
-                        skill:"HTML",
-                        stars: 3,
-                        edit: false,
-                    }, 
-                    {
-                        id: uniqid(),
-                        skill: "CSS",
-                        stars: 3,
-                        edit: false,
-                    },
-                    {
-                        id: uniqid(),
-                        skill: "React.js",
-                        stars: 5,
-                        edit: false,
-                    },
-                    {
-                        id: uniqid(),
-                        skill: "Javascript",
-                        stars: 5,
-                        edit: false,
-                    },
-                    {
-                        id: uniqid(),
-                        skill: "Python",
-                        stars: 2,
-                        edit: false,
-                    }
-                ]
-                })
-               }
-               else{
-                this.setState({
-                    skills: []
-                })
-               }
-        }
-    }
-
-    compareStars(a, b){
+    const compareStars = (a, b) => {
         if ( a.stars < b.stars ){
             return 1;
           }
@@ -73,13 +61,13 @@ class Skills extends Component{
           return 0;
     }
 
-    getSkills(){
-        const sortedSkills = this.state.skills.sort(this.compareStars)
-        const skills = sortedSkills.map( skill => (
-        <div key={skill.id} className={this.props.preview? "skill-preview" : "skill"}>
-            {skill.edit? <input value={this.state.skillInput} onChange={e  => this.setState({skillInput: e.target.value})}/> : <li>{skill.skill}</li>}
+    const getSkills = () => {
+        const sortedSkills = skills.sort(compareStars)
+        const formattedSkills = sortedSkills.map( skill => (
+        <div key={skill.id} className={props.preview? "skill-preview" : "skill"}>
+            {skill.edit? <input value={skillInput} onChange={e  => (setSkillInput(e.target.value))}/> : <li>{skill.skill}</li>}
             {skill.edit? 
-                <select defaultValue={skill.stars} onChange={e  => this.setState({starsInput: Number(e.target.value)})}>
+                <select defaultValue={skill.stars} onChange={e  => setStarsInput(Number(e.target.value))}>
                     <option value={5}>5</option>
                     <option value={4}>4</option>
                     <option value={3}>3</option>
@@ -96,79 +84,71 @@ class Skills extends Component{
                 </div>
             }
             <div className="skills-edit">
-                {this.props.preview? null 
-                : <FontAwesomeIcon className="icon" icon={skill.edit? faSquareCheck: faPenToSquare} size="sm" onClick={ () => this.onClickEditSkill(skill.id)}/>}
-                {this.props.preview? null 
-                : <FontAwesomeIcon className="icon" icon={faTrash} size="sm" onClick={ () => this.onClickDeleteSkill(skill.id)}/>}
+                {props.preview? null 
+                : <FontAwesomeIcon className="icon" icon={skill.edit? faSquareCheck: faPenToSquare} size="sm" onClick={ () => onClickEditSkill(skill.id)}/>}
+                {props.preview? null 
+                : <FontAwesomeIcon className="icon" icon={faTrash} size="sm" onClick={ () => onClickDeleteSkill(skill.id)}/>}
             </div>
         </div>
         ))
-        return skills;
+        return formattedSkills;
     }
 
-    addSkill(){
-        if(this.state.skillInput.length > 0){
+    const addSkill = () => {
+        if(skillInput.length > 0){
             const skill = {
                 id: uniqid(),
-                skill: this.state.skillInput,
-                stars: this.state.starsInput,
+                skill: skillInput,
+                stars: starsInput,
             }
-            this.setState({
-                skills: this.state.skills.concat(skill),
-                edit: !(this.state.edit),
-                skillInput: '',
-                starsInput: 5,
-            })
+            setSkills(skills.concat(skill));
+            setEdit(!edit);
+            setSkillInput('');
+            setStarsInput(5);
         }
     }
 
-    onClickEditSkill(id){
+    const onClickEditSkill = (id) => {
         let index = 0;
-        let updatedSkills = [...this.state.skills];
-        for(let i = 0; i < this.state.skills.length; i++){
-            if(this.state.skills[i].id===id){
+        let updatedSkills = [...skills];
+        for(let i = 0; i < skills.length; i++){
+            if(skills[i].id===id){
                 index = i;
             }
-            else if(this.state.skills[i].edit===true){
+            else if(skills[i].edit===true){
                 let currentSkill = {...updatedSkills[i]};
                 currentSkill.edit = false;
                 updatedSkills[i] = currentSkill;
             }
         }
         let skill = {...updatedSkills[index]};
-        this.setState({
-            skillInput: skill.skill,
-            starsInput: skill.stars
-        })
+        setSkillInput(skill.skill);
+        setStarsInput(skill.stars);
         if(skill.edit){
-            skill.skill = this.state.skillInput
-            skill.stars = this.state.starsInput;
+            skill.skill = skillInput
+            skill.stars = starsInput;
         }
         skill.edit = !skill.edit;
         updatedSkills[index] = skill;
-        this.setState({skills: updatedSkills});
+        setSkills(updatedSkills);
     }
 
-    onClickDeleteSkill(id){
-        const filteredSkills = this.state.skills.filter( skill => skill.id !== id);
-        this.setState({
-            skills : filteredSkills,
-        });
+    const onClickDeleteSkill = (id) => {
+        const filteredSkills = skills.filter( skill => skill.id !== id);
+        setSkills(filteredSkills);
     }
 
-    onClickEdit(){
-        this.setState({
-        edit: (!this.state.edit),
-        });
+    const onClickEdit = () => {
+        setEdit(!edit);
     }
 
-    getIcon(){
-        if(!this.props.preview){
-            if(this.state.edit){
-                return <FontAwesomeIcon className="icon" icon={faSquareCheck} size="xl" onClick={this.onClickEdit.bind(this)}/>
+    const getIcon = () => {
+        if(!props.preview){
+            if(edit){
+                return <FontAwesomeIcon className="icon" icon={faSquareCheck} size="xl" onClick={onClickEdit}/>
             }
             else{
-                return <FontAwesomeIcon className="icon" icon={faPenToSquare} size="xl" onClick={this.onClickEdit.bind(this)}/>
+                return <FontAwesomeIcon className="icon" icon={faPenToSquare} size="xl" onClick={onClickEdit}/>
             }
         }
         else{
@@ -176,23 +156,23 @@ class Skills extends Component{
         }
     }
 
-    getInputForm(){
-        if(!this.props.preview){
-            if(this.state.edit){
+    const getInputForm = () => {
+        if(!props.preview){
+            if(edit){
                 return(
                     <div className="skill-add-container">
                         <div className="skill-add">
                             <label>Skill Name:</label>
-                            <input onChange={e  => this.setState({skillInput: e.target.value})}/>
+                            <input onChange={e  => setSkillInput(e.target.value)}/>
                             <label>Stars:</label>
-                            <select onChange={e  => this.setState({starsInput: Number(e.target.value)})}>
+                            <select onChange={e  => setStarsInput(Number(e.target.value))}>
                                 <option value={5}>5</option>
                                 <option value={4}>4</option>
                                 <option value={3}>3</option>
                                 <option value={2}>2</option>
                                 <option value={1}>1</option>
                             </select>
-                            <button onClick={this.addSkill.bind(this)}>Add</button>
+                            <button onClick={addSkill}>Add</button>
                         </div>
                     </div>
                 )
@@ -200,21 +180,16 @@ class Skills extends Component{
         }
     }
 
-    render(){
-        return(
-            <div className="sidebar">
-                <div className="sidebar-header">
-                    <h2>Skills</h2>
-                    {this.getIcon()}
-                </div>
-                {this.getInputForm()}
-                <div>
-                    {this.getSkills()}
-                </div>
+    return(
+        <div className="sidebar">
+            <div className="sidebar-header">
+                <h2>Skills</h2>
+                {getIcon()}
             </div>
-        )
-    }
-
+            {getInputForm()}
+            <div>
+                {getSkills()}
+            </div>
+        </div>
+    )
 }
-
-export default Skills;
